e66b2acd324c572b801d12a03f22dfc76480bb86
arsal uddin <arsaluddin134@gmail.com>
2023-08-20 21:23:14 +0530

    style-improved

--- backend/DataHandler.js
+++ backend/DataHandler.js
@@ -1,5 +1,64 @@
 
- 
+// const cheerio = require("cheerio");
+// const axios = require("axios");
+
+// const getFromFlipkart = async (req, res) => {
+//   const item = req.query.search;
+
+//   try {
+//     const url = `https://www.flipkart.com/search?q=${item}`;
+
+//     const response = await axios.get(url, {
+//       headers: {
+//         "User-Agent":
+//           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36",
+//       },
+//     });
+//     console.log(response);
+//     const $ = cheerio.load(response.data);
+//     const title = $('div[class="_4rR01T"]').first().text();
+//     const price = $('div[class="_30jeq3 _1_WHN1"]').first().text();
+
+//     // console.log("Title:", title);
+//     // console.log("Price:", price);
+
+//     return res.status(200).json({ title, price });
+//   } catch (error) {
+//     console.error("Error in getFromFlipkart:", error);
+//     res.status(500).json({ error: "An error occurred." });
+//   }
+// };
+
+// const getFromAmazon = async (req, res) => {
+//   const item = req.query.search;
+
+//   try {
+//     const url = `https://www.amazon.com/s?k=${item}`;
+
+//     const { data } = await axios.get(url, {
+//       headers: {
+//         "User-Agent":
+//           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36",
+//       },
+//     });
+    
+//     const $ = cheerio.load(data);
+//     const title = $('span[class="a-size-medium a-color-base a-text-normal"]').first().text();
+
+//     const priceElement = $('span[class="a-offscreen"]').first();
+//     const price = priceElement.text().trim();
+
+//     // console.log("Title:", title);
+//     // console.log("Price:", price);
+
+//     return res.status(200).json({ title, price });
+//   } catch (error) {
+//     console.error("Error in getFromAmazon:", error);
+//     res.status(500).json({ error: "An error occurred." });
+//   }
+// };
+
+// module.exports = { getFromFlipkart, getFromAmazon };
 
 
 const cheerio = require("cheerio");
@@ -19,13 +78,19 @@ const getFromFlipkart = async (req, res) => {
     });
 
     const $ = cheerio.load(response.data);
-    const title = $('div[class="_4rR01T"]').first().text();
-    const price = $('div[class="_30jeq3 _1_WHN1"]').first().text();
+    const products = [];
 
-    console.log("Title:", title);
-    console.log("Price:", price);
+    $('div[data-id]').each((index, element) => {
+      const title = $(element).find('a[rel="noopener noreferrer"]').attr('title');
+      const price = $(element).find('div[class="_30jeq3"]').text();
+      const image = $(element).find('img[class="_396cs4"]').attr('src');
+      const rating = $(element).find('div[class="_3LWZlK"]').text();
+      const description = $(element).find('a[rel="noopener noreferrer"]').text().trim();
 
-    return res.status(200).json({ title, price });
+      products.push({ title, price, image, rating, description });
+    });
+
+    return res.status(200).json(products);
   } catch (error) {
     console.error("Error in getFromFlipkart:", error);
     res.status(500).json({ error: "An error occurred." });
@@ -46,15 +111,19 @@ const getFromAmazon = async (req, res) => {
     });
 
     const $ = cheerio.load(data);
-    const title = $('span[class="a-size-medium a-color-base a-text-normal"]').first().text();
+    const products = [];
 
-    const priceElement = $('span[class="a-offscreen"]').first();
-    const price = priceElement.text().trim();
+    $('div[data-asin]').each((index, element) => {
+      const title = $(element).find('span[class="a-size-medium a-color-base a-text-normal"]').text();
+      const price = $(element).find('span[class="a-offscreen"]').first().text().trim();
+      const image = $(element).find('img[class="s-image"]').attr('src');
+      const rating = $(element).find('span[class="a-icon-alt"]').text();
+      const description = $(element).find('span[class="a-size-base-plus a-color-base a-text-normal"]').text().trim();
 
-    console.log("Title:", title);
-    console.log("Price:", price);
+      products.push({ title, price, image, rating, description });
+    });
 
-    return res.status(200).json({ title, price });
+    return res.status(200).json(products);
   } catch (error) {
     console.error("Error in getFromAmazon:", error);
     res.status(500).json({ error: "An error occurred." });
@@ -62,3 +131,4 @@ const getFromAmazon = async (req, res) => {
 };
 
 module.exports = { getFromFlipkart, getFromAmazon };
+
--- frontend/src/Search.jsx
+++ frontend/src/Search.jsx
@@ -1,66 +1,75 @@
-import { useState } from "react";
-import axios from 'axios'
+import React, { useState } from "react";
+import axios from "axios";
 import Card from "./Card";
-import React from "react";
-import './Search.css'
+import "./Search.css";
 
 const Search = () => {
-  const [search, setSearch] = useState();
-  //flipkart data
-  const [flip_Title, setFlip_Title] = useState("");
-  const [flip_Price, setFlip_Price] = useState("");
-  //amazone data
-  const [amaz_Title, setAmaz_Title] = useState("");
-  const [amaz_Price, setAmaz_Price] = useState("");
+  const [search, setSearch] = useState("");
+  const [flipProducts, setFlipProducts] = useState([]);
+  const [amazProducts, setAmazProducts] = useState([]);
 
   const handleSearch = (e) => {
-    setSearch(e);
+    setSearch(e.target.value);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    //getting data from flipkart
     try {
-      const { data } = await axios.post(
+      const flipResponse = await axios.post(
         `http://localhost:8000/dataFromFlipkart?search=${search}`
       );
-      // console.log("from flipkart ", data);
-      setFlip_Title(data.title);
-      setFlip_Price(data.price);
-    } catch {
-      console.log("error");
-    }
+      setFlipProducts([flipResponse.data]);
 
-    //getting data from amazon
-    try {
-      const { data } = await axios.post(
+      const amazResponse = await axios.post(
         `http://localhost:8000/dataFromAmazon?search=${search}`
       );
-      console.log("from amazon ", data);
-      setAmaz_Title(data.title);
-      setAmaz_Price(data.price);
-    } catch {
-      console.log("error");
+      setAmazProducts([amazResponse.data]);
+    } catch (error) {
+      console.log("Error fetching data:", error);
     }
   };
 
   return (
     <>
       <div className="search-bar">
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <input
             name="search"
             placeholder="Enter the product name"
-            onChange={(e) => handleSearch(e.target.value)}
-          ></input>
-          <button type="submit" onClick={handleSubmit}>
-            Search
-          </button>
+            onChange={(e) => handleSearch(e)}
+          />
+          <button type="submit">Search</button>
         </form>
       </div>
-      <Card title={flip_Title} price={flip_Price} />
-      <Card title={amaz_Title} price={amaz_Price} />
+      <div className="comparison-container">
+        <div className="flipkart-products">
+          <h2>Products from Flipkart</h2>
+          {flipProducts.map((product, index) => (
+            <Card
+              key={index}
+              image={product.image}
+              title={product.title}
+              price={product.price}
+              rating={product.rating}
+              description={product.description}
+            />
+          ))}
+        </div>
+        <div className="amazon-products">
+          <h2>Products from Amazon</h2>
+          {amazProducts.map((product, index) => (
+            <Card
+              key={index}
+              image={product.image}
+              title={product.title}
+              price={product.price}
+              rating={product.rating}
+              description={product.description}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
